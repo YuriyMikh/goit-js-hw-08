210b5b0561334e20611a2f7a3c0b36cabff51838
YuriyMikh <pamukkale1717@gmail.com>
2023-04-09 21:14:02 +0300

    add VimeoPlayer and Lodash throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import _ from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', _(onPlay, 1000));
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(data) {
+
+}
+
+//-----
+// document.addEventListener(
+//   'scroll',
+//   _.throttle(
+
+//1 параметр функция:
+// () => {console.log('Scroll handler call every 300ms');},
+
+//2йпараметр задержка:
+// 300)
+// );
