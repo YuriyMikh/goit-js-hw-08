039abd3f2faa8dbcbd5b1913828d469d6956cce0
YuriyMikh <pamukkale1717@gmail.com>
2023-04-16 23:05:17 +0300

    upd task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,21 +4,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
-
 function onPlay(data) {
-
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 }
 
-//-----
-// document.addEventListener(
-//   'scroll',
-//   _.throttle(
-
-//1 параметр функция:
-// () => {console.log('Scroll handler call every 300ms');},
-
-//2йпараметр задержка:
-// 300)
-// );
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
