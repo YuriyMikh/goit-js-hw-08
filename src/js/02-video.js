822975e0895a83cf4fe5edd565e032211d2d1c6c
YuriyMikh <pamukkale1717@gmail.com>
2023-05-19 00:52:18 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
